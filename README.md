9fcd51ca3d5f8aa49c6df904ba7c449761ec0f46
Nikhil Ostwal <nikhilostwal1996@gmail.com>
2022-12-07 20:00:30 +0100

    Update Webapp

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Olis Customer Satisfaction Prediction
 Objective of this case study is centered around predicting customer satisfaction with a product which can be deduced after predicting the product rating a user would rate after he makes a purchase.
 
+The dataset used is a Brazilian ecommerce public dataset of orders made at Olist Store. The dataset has information of 100k orders from 2016 to 2018 made at multiple marketplaces in Brazil. Its features allows viewing an order from multiple dimensions: from order status, price, payment and freight performance to customer location, product attributes and finally reviews written by customers.
+
 The dataset can be found [here](https://www.kaggle.com/datasets/olistbr/brazilian-ecommerce).
--- pages/Data_Insights.py
+++ pages/Data_Insights.py
@@ -2,10 +2,12 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv("preprocessed_data.csv")
+st.set_page_config(layout="wide")
+st.markdown("## Here you can get an quick overview of the clean dataset")
 
-st.markdown("## Here you can explore the data set. ")
-
-st.markdown("Select the type of insight to look into from the left panel.")
+col1, col2, col3, col4 = st.columns(4)
+        
+st.markdown("Select the choice of insight from the left panel:")
 
 if st.sidebar.button('Dataset Quick Look'):
     st.subheader('Dataset Quick Look:')
@@ -14,11 +16,23 @@ if st.sidebar.button('Statistical Description'):
     st.subheader('Statistical Data Descripition')
     st.write(df.describe())
 if st.sidebar.button('Missing Values'):
-    st.subheader('Missing values')
+    st.subheader('Sum of Missing values in each column')
     st.write(df.isnull().sum())
 if st.sidebar.button('Corelation'):
     st.subheader('Corelation between the features')
-    st.write(df.corr())    
+    st.write(df.corr())
+
+
+
+# with col1:
+#     st.header("A column")
+#     st.button('BUT 1')
+#     st.image("https://static.streamlit.io/examples/cat.jpg")
+
+# with col2:
+#     st.header("A col2")
+#     st.button('But 2')
+#     st.image("https://static.streamlit.io/examples/dog.jpg")
 
 
 
